5afd37ef0fce92468e458175a4cb10d5462572df
Tsuki <i@sukitsuki.com>
2017-08-23 16:44:32 +0800

    Add page and post and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,32 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {BodyComponent} from './body/body.component';
 import {GlobalDataService} from './service/globaldata.service';
 import {FooterComponent} from './footer/footer.component';
 import {MobileNavComponent} from './mobile-nav/mobile-nav.component';
 import {AfterFooterComponent} from './after-footer/after-footer.component';
 import {RouterModule, Routes} from '@angular/router';
+import {PostComponent} from './post/post.component';
+import {PageComponent} from './page/page.component';
 
 const appRoutes: Routes = [
-  {path: 'crisis-center', component: AppComponent},
+  {path: '', component: PostComponent},
+  {path: 'about', component: PageComponent},
+  {path: 'archives', component: PageComponent},
+  {path: 'tags', component: PageComponent},
+  {path: 'picture', component: PageComponent},
+  {path: 'works', component: PageComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent,
     FooterComponent,
     MobileNavComponent,
-    AfterFooterComponent
+    AfterFooterComponent,
+    PostComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/post/post.component.spec.ts
+++ src/app/post/post.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BodyComponent } from './body.component';
+import { PostComponent } from './post.component';
 
-describe('BodyComponent', () => {
-  let component: BodyComponent;
-  let fixture: ComponentFixture<BodyComponent>;
+describe('PostComponent', () => {
+  let component: PostComponent;
+  let fixture: ComponentFixture<PostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BodyComponent ]
+      declarations: [ PostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BodyComponent);
+    fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
