47b1262b46d2644fac46c120d976add02c386ab5
Tsuki <i@sukitsuki.com>
2017-08-23 15:40:32 +0800

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {BodyComponent} from './body/body.component';
 import {GlobalDataService} from './service/globaldata.service';
-import { FooterComponent } from './footer/footer.component';
-import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
-import { AfterFooterComponent } from './after-footer/after-footer.component';
+import {FooterComponent} from './footer/footer.component';
+import {MobileNavComponent} from './mobile-nav/mobile-nav.component';
+import {AfterFooterComponent} from './after-footer/after-footer.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'crisis-center', component: AppComponent},
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +24,11 @@ import { AfterFooterComponent } from './after-footer/after-footer.component';
     AfterFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [GlobalDataService],
   bootstrap: [AppComponent]
